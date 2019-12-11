6722e95e161dd1809724626293bacebcc06e0e68
tianbin <nibnait@tianbindeMacBook-Pro.local>
2019-12-11 09:22:49 +0800

    first commit

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,21 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by tianbin on 2019/12/11.
+//  Copyright © 2019 tianbin. All rights reserved.
+//
+
+import SwiftUI
+
+struct Landmark: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Landmark_Previews: PreviewProvider {
+    static var previews: some View {
+        Landmark()
+    }
+}
